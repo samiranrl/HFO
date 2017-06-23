5aae003545727b3ddfb1ce8c1826964b76241018
samiranrl <samiranroyofficial@gmail.com>
2017-06-23 20:56:04 +0530

    install2

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install build essential
+sudo apt-get install build-essential
 sudo apt-get install cmake libboost-filesystem-dev libboost-system-dev flex
 sudo apt-get install libpng-dev
 sudo apt-get install zlib1g-dev
