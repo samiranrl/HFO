5d077e9313b2bdae32a5f1cdac8a76c9478d0f4a
samiranrl <samiranroyofficial@gmail.com>
2017-06-23 20:01:34 +0530

    added install.sh and start.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get install cmake libboost-filesystem-dev libboost-system-dev flex
+sudo apt-get install libpng-dev
+sudo apt-get install zlib1g-dev
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SOCCERWINDOW=False ..
+make
+sudo make install
+cd ..
+cd example/sarsa_offense/
+make
+cd ..
+cd ..
---
+++ start.sh
@@ -0,0 +1,29 @@
+for i in `seq $2 100`
+
+do
+
+a=$1
+b=$(($1+1))
+c=$(($1+2))
+
+( cd example/sarsa_offense && cp weights_$a* weights_backup && cp weights_$b* weights_backup && cp weights_$c* weights_backup ) 
+
+echo "Train $i" >> train$1.txt
+stdbuf -oL ./bin/HFO  --offense-agents 3 --defense-npcs 2 --headless --no-logging --trials 1000 --seed $1 --port $1>> train$1.txt &
+sleep 5
+( cd example/sarsa_offense && ./high_level_sarsa_agent --learnRate .05 --numAgents 3 --numEpisodes 1000 --basePort $1 ) 
+
+
+
+wait
+
+echo "Test $i" >> test$1.txt
+stdbuf -oL ./bin/HFO  --offense-agents 3 --defense-npcs 2 --headless --no-logging --trials 10000 --seed $1 --port $1 >> test$1.txt &
+sleep 5
+( cd example/sarsa_offense && ./high_level_sarsa_agent --learnRate 0 --eps 0 --numAgents 3 --numEpisodes 10000 --basePort $1 )
+wait
+
+
+
+
+done
