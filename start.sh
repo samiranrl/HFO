4c3f2828af835a765efe01caceaca9858f2c248d
Samiran Roy <samiranrl@users.noreply.github.com>
2017-06-25 15:20:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,9 +18,9 @@ sleep 5
 wait
 
 echo "Test $i" >> test$1.txt
-stdbuf -oL ./bin/HFO  --offense-agents 3 --defense-npcs 2 --headless --no-logging --trials 15000 --seed $1 --port $1 >> test$1.txt &
+stdbuf -oL ./bin/HFO  --offense-agents 3 --defense-npcs 2 --headless --no-logging --trials 5000 --seed $1 --port $1 >> test$1.txt &
 sleep 5
-( cd example/sarsa_offense && ./high_level_sarsa_agent --learnRate 0 --eps 0 --numAgents 3 --numEpisodes 15000 --basePort $1 )
+( cd example/sarsa_offense && ./high_level_sarsa_agent --learnRate 0 --eps 0 --numAgents 3 --numEpisodes 5000 --basePort $1 )
 wait
 
 
